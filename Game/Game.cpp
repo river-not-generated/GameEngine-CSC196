e98d76ff5fae49cfda35105e7108ccb1efff0ae5
river-not-generated <river.unroe@gmail.com>
2026-07-01 10:47:32 -0600

    Added another rectangle and some effects

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -27,7 +27,15 @@ int main()
     bool quit = false;
 
     // Define a rectangle
-    SDL_FRect greenSquare{ 270, 190, 200, 200 };
+    SDL_FRect greenSquare{ 270, 190, 150, 150 };
+    SDL_FRect colouredSquare{ 600, 600, 30, 30 };
+    SDL_Color colour{ 30, 255, 150, 255 };
+
+    bool increasing = false;
+    bool rIncrease = true;
+    bool gIncrease = false;
+    bool bIncrease = false;
+    int increment = 0;
 
     while (!quit) {
         while (SDL_PollEvent(&e)) {
@@ -36,11 +44,35 @@ int main()
             }
         }
 
+        if (greenSquare.w >= 200) {
+            increasing = false;
+        }
+        if (greenSquare.w <= 5) {
+            increasing = true;
+        }
+        increment = (increment == 5 ? 0 : increment + 1);
+
+        if (colour.r == 255) rIncrease = false;
+        if (colour.r == 0) rIncrease = true;
+        if (colour.g == 255) gIncrease = false;
+        if (colour.g == 0) gIncrease = true;
+        if (colour.b == 255) bIncrease = false;
+        if (colour.b == 0) bIncrease = true;
+
+        if (increment == 0) {
+            greenSquare.w = (increasing ? greenSquare.w + 1 : greenSquare.w - 1);
+            greenSquare.h = (increasing ? greenSquare.h + 1 : greenSquare.h - 1);
+            colour.r = (rIncrease ? colour.r + 1 : colour.r - 1);
+            colour.g = (gIncrease ? colour.g + 1 : colour.g - 1);
+            colour.b = (bIncrease ? colour.b + 1 : colour.b - 1);
+        }
+
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
         SDL_RenderClear(renderer); // Clear the renderer
 
-        // new stuff
-        SDL_RenderPoint(renderer, 20, 20);
+        SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a);
+        SDL_RenderFillRect(renderer, &colouredSquare);
 
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Set render draw color to green
         SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
