be013fca6111da4f98566262d6d31317ecd3b939
River Unroe <river.unroe@gmail.com>
2026-07-01 09:52:00 -0600

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,12 @@
+// Engine.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+// TODO: This is an example of a library function
+void fnEngine()
+{
+	std::cout << "library connected" << std::endl;
+}
